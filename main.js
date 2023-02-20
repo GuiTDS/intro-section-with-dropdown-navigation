78b2859327b3cbe65fe7ecd01acadb532a419f7e
Guilherme Sanches <guitdsanches@gmail.com>
2023-02-20 15:16:21 -0300

    Addition of main section

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const btnOpenMenu = document.querySelector('.lateral-menu-btn');
 const lateralMenu = document.querySelector('.lateral-menu');
+const dropdown = document.querySelectorAll('.dropdown');
 
 btnOpenMenu.addEventListener("click", () => {
     document.body.classList.toggle('menu-open');
     lateralMenu.classList.toggle('lateral-menu-active');
+})
+
+dropdown.forEach( btn => {
+    btn.addEventListener("click", () => {
+        btn.parentElement.classList.toggle('dropdown-active');
+    })
 })
